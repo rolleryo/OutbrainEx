5cde6eac4dc824d7a8e7391ab72df95b10b514e0
rolleryo <yoavroller@gmail.com>
2022-12-06 16:13:20 +0200

    create getTemp() function

--- Solution/OutRainAPI.py
+++ Solution/OutRainAPI.py
@@ -5,28 +5,32 @@ from requests import get
 import ipinfo # get ip based location
 import json
 
-# get external ip using http request from ipify
-external_ip = get('https://api.ipify.org').content.decode('utf8')
+def getTemp():
+    # get external ip using http request from ipify
+    external_ip = get('https://api.ipify.org').content.decode('utf8')
 
-# get city & country according to external ip using ipinfo library
-ipinfo_token = 'd5a13af3bebba0'
-handler = ipinfo.getHandler(ipinfo_token) # create object handler 
-ipinfo_details = handler.getDetails(external_ip) # use getDetails property
-city = ipinfo_details.city
-country = ipinfo_details.country
+    # get city & country according to external ip using ipinfo library
+    ipinfo_token = 'd5a13af3bebba0'
+    handler = ipinfo.getHandler(ipinfo_token) # create object handler 
+    ipinfo_details = handler.getDetails(external_ip) # use getDetails property
+    city = ipinfo_details.city
+    country = ipinfo_details.country
 
-### get temp with respect to city & country 
-weatherReq = requests.get('http://api.openweathermap.org/data/2.5/weather?units=metric&q='+city+','+country+'&APPID=0a1e14ce198f208c5665f1b1a6bb4879').text
+    ### get temp with respect to city & country 
+    weatherReq = requests.get('http://api.openweathermap.org/data/2.5/weather?units=metric&q='+city+','+country+'&APPID=0a1e14ce198f208c5665f1b1a6bb4879').text
 
-### parse response using json
-data = json.loads(weatherReq)
-temp=data['main']['temp']
+    ### parse response using json
+    data = json.loads(weatherReq)
+    temp=data['main']['temp']
 
-### format response as json 
-value = {
-        "city": city,
-        "country": country,
-        "temp": temp,
-        }
-jsonString = json.dumps(value)
-print(jsonString)
+    ### format response as json 
+    value = {
+            "city": city,
+            "country": country,
+            "temp": temp,
+            }
+    jsonString = json.dumps(value)
+    
+    return (jsonString)
+
+print(getTemp())
