b89aa0a682a96a33d1f52142a5fba184b5877758
rolleryo <yoavroller@gmail.com>
2022-12-11 07:27:01 +0200

    erase uneeded material

--- Solution/json/SolutionToParseListToJson.py
+++
@@ -1,5 +0,0 @@
-
-input_list = ['name SP1, status Offline, size 4764771 MB']
-output_list = [{key: value for key, value in [item.split() for item in item.split(',')] for item in input_list}]
-print(output_list)
-# Output: [{"name": "SP1", "status": "Offline", "size": "4764771 MB"}]
